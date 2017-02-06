3140d8e76914cd4970a7edbe83e3133fe33bebed
duck105 <jacky87810@gmail.com>
2017-02-06 17:14:36 +0800

    Add score column to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+	before_filter :configure_permitted_parameters, if: :devise_controller?
+
+
+	protected
+
+  def configure_permitted_parameters
+   	devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:score, :email, :password, :password_confirmation) }
+   	devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:current_password,:score, :email, :password, :password_confirmation)}
+  end
+
 end
