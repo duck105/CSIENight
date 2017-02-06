c3eb755ed5c2d228e881d0a6b5ec239a11c7d237
duck105 <jacky87810@gmail.com>
2017-02-06 16:44:35 +0800

    Add user

--- Gemfile
+++ Gemfile
@@ -35,6 +35,11 @@ gem 'jbuilder', '~> 2.5'
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 gem 'mysql2'
+gem 'devise', '~> 4.2'
+gem 'mini_magick', "~> 4.6.0"
+gem 'carrierwave', '~> 1.0'
+gem 'omniauth', '~> 1.3.1'
+gem 'omniauth-facebook', '~> 4.0.0'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170205181106) do
+ActiveRecord::Schema.define(version: 20170206073344) do
 
   create_table "questions", force: :cascade do |t|
     t.datetime "created_at",  null: false
@@ -28,6 +28,24 @@ ActiveRecord::Schema.define(version: 20170205181106) do
     t.string   "answer"
     t.integer  "question_id"
     t.index ["question_id"], name: "index_submissions_on_question_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.integer  "score"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
