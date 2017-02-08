beeceeb1a70056828bd0e098d1f3acf0998ac8b8
duck105 <jacky87810@gmail.com>
2017-02-08 13:52:56 +0800

    Remove score edit function

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,8 @@ class ApplicationController < ActionController::Base
 	protected
 
   def configure_permitted_parameters
-   	devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:score, :email, :password, :password_confirmation) }
-   	devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:score, :email, :password, :password_confirmation)}
+   	devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:email, :password, :password_confirmation) }
+   	devise_parameter_sanitizer.permit(:account_update) {|u| u.permit(:email, :password, :password_confirmation)}
   end
 
 end
