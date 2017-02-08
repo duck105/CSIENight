306ac5c8b9c5e1a5b99f669c09f9b0222ed2900c
duck105 <jacky87810@gmail.com>
2017-02-08 16:13:46 +0800

    Make rank more elegant

--- app/controllers/rank_controller.rb
+++ app/controllers/rank_controller.rb
@@ -1,5 +1,5 @@
 class RankController < ApplicationController
 	def index
-		@users = User.order(:score => :desc).limit(20)
+		@users = User.order(:score => :desc, :email => :asc).limit(20)
 	end
 end
