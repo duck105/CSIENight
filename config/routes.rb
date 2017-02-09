bb207d90d6cebbe46c76eafcc93be6e80f0c246e
duck105 <jacky87810@gmail.com>
2017-02-09 14:30:03 +0800

    Init Administrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+resources :categories
+resources :infors
+resources :judges
+resources :options
+resources :questions
+resources :submissions
+
+    root to: "users#index"
+  end
+
   devise_for :users, controllers: { registrations: "users/registrations" , omniauth_callbacks: "users/omniauth_callbacks", sessions: 'users/sessions'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html  
   resources :questions do
