f3c3bb621f5f15bc0132e4e460b0f00104abd2dc
duck105 <jacky87810@gmail.com>
2017-02-26 16:00:10 +0800

    Add hi_user button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def render_hi_user
+    if flash[:notice] == "Successfully authenticated from Facebook account." && user_signed_in? 
+      return link_to("Hi!#{current_user.name}", edit_user_registration_path, class: "pure-menu-link")
+    end
+  end
 end
