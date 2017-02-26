81ceb119dabb903dea48db30f973c26f00325e60
duck105 <jacky87810@gmail.com>
2017-02-27 00:45:58 +0800

    Fix bug and remove admin login form

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -3,7 +3,7 @@ module QuestionsHelper
     return ((Time.now -  question.updated_at)/86400).to_i
   end
   def question_check_mark(question)
-    if Judge.exists?(["question_id = ? AND user_id = ? AND state = ?", question.id, current_user.id, 1])
+    if user_signed_in? && Judge.exists?(["question_id = ? AND user_id = ? AND state = ?", question.id, current_user.id, 1])
       return image_tag("check1.png", id: "check")
     end
   end
