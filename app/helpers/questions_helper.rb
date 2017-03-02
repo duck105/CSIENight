e834e75a7824b31e746d5de821c9b8b737a94a6e
duck105 <jacky87810@gmail.com>
2017-03-03 00:35:16 +0800

    Make css great again and fix new class bug

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,6 +1,6 @@
 module QuestionsHelper
   def new_or_not(question)
-    return ((Time.now -  question.updated_at)/86400).to_i
+    return ((Time.current -  question.updated_at)/86400).to_i
   end
   def question_check_mark(question)
     if user_signed_in? && Judge.exists?(["question_id = ? AND user_id = ? AND state = ?", question.id, current_user.id, 1])
